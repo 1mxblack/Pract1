f9996f111ad3c7c73e5a47527cc5d9ffa121987c
Макс <prostomax01@icloud.com>
2023-03-13 14:08:06 +0300

    1

---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OldMlad
+{
+    class Person
+    {
+        public int year { get; set; }
+        public string name { get; set; }
+
+        public static bool operator <(Person c1, Person c2)
+        {
+            return c1.year < c2.year;
+        }
+
+        public static bool operator >(Person c1, Person c2)
+        {
+            return c1.year > c2.year;
+        }
+
+        public static bool operator ==(Person c1, Person c2)
+        {
+            return c1.year == c2.year;
+        }
+        public static bool operator !=(Person c1, Person c2)
+        {
+            return c1.year != c2.year;
+        }
+
+    }
+}
---
+++ СтаршеМладше/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OldMlad
+{
+    
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Person man1 = new Person { year = 1967, name = "игорь" };
+            Person man2 = new Person { year = 1967, name = "макс" };
+            Person man3 = new Person { year = 1967, name = "олег" };
+            bool result = man1 > man3;
+            bool result1 = man1 < man3;
+            bool result2 = man1 == man3;
+            bool result3 = man1 != man3;
+            Console.WriteLine("{0} {1} {2}", result, result1, result2);
+        }
+    }
+}
+
